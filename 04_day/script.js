2453165a3d1e751ded6808d68255a22a7ed2d399
AlexReshetnik <reshetnik262@gmail.com>
2021-12-04 19:05:10 +0200

    четвірку забув добавити

--- 04_day/script.js
+++ 04_day/script.js
@@ -2,7 +2,7 @@ const HEIGHTc = 30;
 const WIDTHc = 30;
 const WIDTHcube = 20;
 var board = document.querySelector("#board");
-board.style.width = HEIGHTc * (WIDTHcube+())+"px";
+board.style.width = HEIGHTc * (WIDTHcube + (4)) + "px";
 for (let i = 0; i < HEIGHTc * WIDTHc; i++) {
     const cubes = document.createElement("div");
     cubes.classList.add("cube")
@@ -13,12 +13,12 @@ for (let i = 0; i < HEIGHTc * WIDTHc; i++) {
     board.append(cubes);
 }
 var count = 0;
-var colors = ["red", "rgb(0, 255, 0)", "blue", "blueviolet", "cyan", "deeppink","rgb(255, 0, 157)"];
+var colors = ["red", "rgb(0, 255, 0)", "blue", "blueviolet", "cyan", "deeppink", "rgb(255, 0, 157)"];
 function addColor(cube) {
-   
+
     cube.style.transitionDuration = "0s";
     cube.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    if (count == 3) { count = 0;}
+    if (count == 3) { count = 0; }
 }
 function removeColor(cube) {
     cube.style.transitionDuration = "0.5s";
